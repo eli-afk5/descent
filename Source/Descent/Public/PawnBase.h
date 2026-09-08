c90fe05816154a9b45cfde9e527bf3a7fe2ee053
Elisa <elisaarata5@gmail.com>
2026-09-08 09:54:01 +0200

    added EnergyCounter and checks energy when firing lasers

--- Source/Descent/Public/PawnBase.h
+++ Source/Descent/Public/PawnBase.h
@@ -20,6 +20,8 @@ public:
 	float RotationSpeed = 200.0f;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Variables")
 	bool bHasKey = false;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Variables")
+	int EnergyCounter = 100;
 	
 	// Sets default values for this pawn's properties
 	APawnBase();
